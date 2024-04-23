b38f60804b9515cfed70806005aae0b05ff16623
MaxPredathor <max.gilli98@gmail.com>
2024-04-23 16:08:47 +0200

    Esercizio completato

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        public enum Azioni
+        {
+            SommaInteri,
+            SommaDoublen,
+            DifferenzaInteri,
+            DifferenzaDouble,
+            MoltiplicaInteri,
+            MoltiplicaDouble,
+            ValoreAssolutoInteri,
+            ValoreAssolutoDouble,
+            MinimoInteri,
+            MinimoDouble,
+            MassimoInteri,
+            MassimoDouble,
+            AllaPotenza
+        }
+        public static int Somma(int num, int num2)
+        {
+            return num + num2;
+        }
+
+        public static double Somma(double num, double num2)
+        {
+            return num + num2;
+        }
+        public static int Differenza(int num, int num2)
+        {
+            return num - num2;
+        }
+
+        public static double Differenza(double num, double num2)
+        {
+            return num - num2;
+        }
+        public static int Moltiplica(int num, int num2)
+        {
+            return num * num2;
+        }
+
+        public static double Moltiplica(double num, double num2)
+        {
+            return num * num2;
+        }
+        public static int ValoreAssoluto(int num)
+        {
+            return Math.Abs(num);
+        }
+
+        public static double ValoreAssoluto(double num)
+        {
+            return Math.Abs(num);
+        }
+        public static string Minimo(int num, int num2)
+        {
+            if (num < num2)
+                return $"Il numero mineore e' {num}";
+            else if (num > num)
+                return $"Il numero mineore e' {num2}";
+            else
+                return "I Numeri sono uguali";
+        }
+        public static string Minimo(double num, double num2)
+        {
+            if (num < num2)
+                return $"Il numero mineore e' {num}";
+            else if (num > num)
+                return $"Il numero mineore e' {num2}";
+            else
+                return "I Numeri sono uguali";
+        }
+       
+        public static string Massimo(int num, int num2)
+        {
+            if (num > num2)
+                return $"Il numero maggiore e' {num}";
+            else if (num2 > num)
+                return $"Il numero maggiore e' {num2}";
+            else
+                return "I Numeri sono uguali";
+        }
+        public static string Massimo(double num, double num2)
+        {
+            if (num > num2)
+                return $"Il numero maggiore e' {num}";
+            else if (num2 > num)
+                return $"Il numero maggiore e' {num2}";
+            else
+                return "I Numeri sono uguali";
+        }
+        // Polimorfismo
+        // SuperFunzione Minimo
+        public static string SuperMinimo<T>(T num, T num2) where T : IComparable<T>
+        {
+            if (num.CompareTo(num2) < 0)
+                return $"Il numero mineore e' {num}";
+            else if (num.CompareTo(num2) > 0)
+                return $"Il numero mineore e' {num2}";
+            else
+                return "I Numeri sono uguali";
+        }
+        // SuperFunzione Massimo
+        public static string SuperMassimo<T>(T num, T num2) where T : IComparable<T>
+        {
+            if (num.CompareTo(num2) > 0)
+                return $"Il numero maggiore e' {num}";
+            else if (num.CompareTo(num2) < 0)
+                return $"Il numero maggiore e' {num2}";
+            else
+                return "I Numeri sono uguali";
+        }
+        //Bonus
+        public static double AllaPotenza(int num, int esponente)
+        {
+            return Math.Pow(num, esponente);
+        }
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -1,10 +1,143 @@
-﻿namespace csharp_calcolatrice
+﻿using static csharp_calcolatrice.CalcoliHelper;
+using System;
+
+namespace csharp_calcolatrice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            while (true)
+            {
+                Console.WriteLine("Inserisci il numero corrispondente all'azione desiderata");
+                int counter = 1;
+                foreach (Azioni azione in Enum.GetValues(typeof(Azioni))) 
+                {
+                    Console.WriteLine($"-{counter} {azione}");
+                    counter++;
+                }
+                string inputUtente = Console.ReadLine();
+                switch (inputUtente)
+                {
+                    case "1":
+                        Console.WriteLine("Hai scelto SommaInteri:");
+                        Console.WriteLine("Inserire il primo numero intero");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero intero");
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        int sommaInt = CalcoliHelper.Somma(num1, num2);
+                        Console.WriteLine($"Il risultato e' {sommaInt}");
+                        break;
+                    case "2":
+                        Console.WriteLine("Hai scelto SommaDouble:");
+                        Console.WriteLine("Inserire il primo numero double");
+                        double n1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero double");
+                        double n2 = Convert.ToDouble(Console.ReadLine());
+                        double sommaDouble = CalcoliHelper.Somma(n1, n2);
+                        Console.WriteLine($"Il risultato e' {sommaDouble}");
+                        break;
+                    case "3":
+                        Console.WriteLine("Hai scelto DifferenzaInteri:");
+                        Console.WriteLine("Inserire il primo numero intero");
+                        int diff1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero intero");
+                        int diff2 = Convert.ToInt32(Console.ReadLine());
+                        int diffInt = CalcoliHelper.Differenza(diff1, diff2);
+                        Console.WriteLine($"Il risultato e' {diffInt}");
+                        break;
+                    case "4":
+                        Console.WriteLine("Hai scelto DifferenzaDouble:");
+                        Console.WriteLine("Inserire il primo numero double");
+                        double diff3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero double");
+                        double diff4 = Convert.ToDouble(Console.ReadLine());
+                        double diffDouble = CalcoliHelper.Differenza(diff3, diff4);
+                        Console.WriteLine($"Il risultato e' {diffDouble}");
+                        break;
+                    case "5":
+                        Console.WriteLine("Hai scelto MoltiplicaInteri:");
+                        Console.WriteLine("Inserire il primo numero intero");
+                        int molt1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero intero");
+                        int molt2 = Convert.ToInt32(Console.ReadLine());
+                        int moltInt = CalcoliHelper.Moltiplica(molt1, molt2);
+                        Console.WriteLine($"Il risultato e' {moltInt}");
+                        break;
+                    case "6":
+                        Console.WriteLine("Hai scelto MoltiplicaDouble:");
+                        Console.WriteLine("Inserire il primo numero double");
+                        double molt3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero double");
+                        double molt4 = Convert.ToDouble(Console.ReadLine());
+                        double moltDouble = CalcoliHelper.Moltiplica(molt3, molt4);
+                        Console.WriteLine($"Il risultato e' {moltDouble}");
+                        break;
+                    case "7":
+                        Console.WriteLine("Hai scelto ValoreAssolutoInteri:");
+                        Console.WriteLine("Inserire il numero intero di cui si vuole il valore assoluto");
+                        int vass1 = Convert.ToInt32(Console.ReadLine());
+                        int vassInt = CalcoliHelper.ValoreAssoluto(vass1);
+                        Console.WriteLine($"Il risultato e' {vassInt}");
+                        break;
+                    case "8":
+                        Console.WriteLine("Hai scelto ValoreAssolutoDouble:");
+                        Console.WriteLine("Inserire il numero double di cui si vuole il valore assoluto");
+                        double vass2 = Convert.ToDouble(Console.ReadLine());
+                        double vassDouble = CalcoliHelper.ValoreAssoluto(vass2);
+                        Console.WriteLine($"Il risultato e' {vassDouble}");
+                        break;
+                    case "9":
+                        Console.WriteLine("Hai scelto MinimoInteri:");
+                        Console.WriteLine("Inserire il primo numero intero");
+                        int min1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero intero");
+                        int min2 = Convert.ToInt32(Console.ReadLine());
+                        string minInt = CalcoliHelper.Minimo(min1, min2);
+                        Console.WriteLine(minInt);
+                        break;
+                    case "10":
+                        Console.WriteLine("Hai scelto MinimoDouble:");
+                        Console.WriteLine("Inserire il primo numero double");
+                        double min3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero double");
+                        double min4 = Convert.ToDouble(Console.ReadLine());
+                        string minDouble = CalcoliHelper.Minimo(min3, min4);
+                        Console.WriteLine(minDouble);
+                        break;
+                    case "11":
+                        Console.WriteLine("Hai scelto MassimoInteri:");
+                        Console.WriteLine("Inserire il primo numero intero");
+                        int max1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero intero");
+                        int max2 = Convert.ToInt32(Console.ReadLine());
+                        string maxInt = CalcoliHelper.Massimo(max1, max2);
+                        Console.WriteLine(maxInt);
+                        break;
+                    case "12":
+                        Console.WriteLine("Hai scelto MassimoDouble:");
+                        Console.WriteLine("Inserire il primo numero double");
+                        double max3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Inserire il secondo numero double");
+                        double max4 = Convert.ToDouble(Console.ReadLine());
+                        string maxDouble = CalcoliHelper.Massimo(max3, max4);
+                        Console.WriteLine(maxDouble);
+                        break;
+                    case "13":
+                        Console.WriteLine("Hai scelto AllaPotenza:");
+                        Console.WriteLine("Inserire il numero da esporre alla potenza");
+                        int pow1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Inserire la potenza al quale esporre il numero precedente");
+                        int pow2 = Convert.ToInt32(Console.ReadLine());
+                        double powDouble = CalcoliHelper.AllaPotenza(pow1, pow2);
+                        Console.WriteLine($"Il rusultato e' {powDouble}");
+                        break;
+                    default:
+                        Console.WriteLine("Azione non supportata");
+                        break;
+                }
+                Console.WriteLine("-----------------------------------------------------------------");
+            }
+        }           
     }
 }
